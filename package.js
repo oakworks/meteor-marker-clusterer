cde6272d1294fcedbff59b894ee9b10161cf2b6f
Rob Lingle <rob@oak.works>
2016-05-29 12:43:35 -0400

    use `api.addAssets` instead of `addFiles`

--- package.js
+++ package.js
@@ -8,19 +8,19 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('markerclusterer.js', 'client');
-  api.addFiles('images/conv30.png', 'client');
-  api.addFiles('images/conv40.png', 'client');
-  api.addFiles('images/conv50.png', 'client');
-  api.addFiles('images/heart30.png', 'client');
-  api.addFiles('images/heart40.png', 'client');
-  api.addFiles('images/heart50.png', 'client');
-  api.addFiles('images/m1.png', 'client');
-  api.addFiles('images/m2.png', 'client');
-  api.addFiles('images/m3.png', 'client');
-  api.addFiles('images/m4.png', 'client'); 
-  api.addFiles('images/m5.png', 'client');
-  api.addFiles('images/people35.png', 'client');
-  api.addFiles('images/people45.png', 'client');
-  api.addFiles('images/people55.png', 'client');
+  api.addAssets('markerclusterer.js', 'client');
+  api.addAssets('images/conv30.png', 'client');
+  api.addAssets('images/conv40.png', 'client');
+  api.addAssets('images/conv50.png', 'client');
+  api.addAssets('images/heart30.png', 'client');
+  api.addAssets('images/heart40.png', 'client');
+  api.addAssets('images/heart50.png', 'client');
+  api.addAssets('images/m1.png', 'client');
+  api.addAssets('images/m2.png', 'client');
+  api.addAssets('images/m3.png', 'client');
+  api.addAssets('images/m4.png', 'client');
+  api.addAssets('images/m5.png', 'client');
+  api.addAssets('images/people35.png', 'client');
+  api.addAssets('images/people45.png', 'client');
+  api.addAssets('images/people55.png', 'client');
 });
